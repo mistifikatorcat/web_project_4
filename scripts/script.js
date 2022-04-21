aa9b08bcaea3948ae18b434a0b0a70fc0bc34af2
Daniel Evgrafov <danyaevgrafov@gmail.com>
2022-04-21 22:17:14 +0300

    develop addForm

--- scripts/script.js
+++ scripts/script.js
@@ -37,16 +37,15 @@ let formEdit = document.querySelector(".edit");
 
     function openEditForm(){
         formEdit.classList.toggle("edit_enabled");
-        let fullname = document.querySelector(".profile__name"); //данные из самого сайта
+        let fullname = document.querySelector(".profile__name"); 
         let job = document.querySelector(".profile__category");
-        let nameInput = document.getElementById("name"); //данные из формы
+        let nameInput = document.getElementById("name"); 
         let jobInput = document.getElementById("category"); 
 
-        nameInput.value = fullname.textContent;  //автозаполнение формы содержимым из хтмл
+        nameInput.value = fullname.textContent;  
         jobInput.value = job.textContent;
 
-        //textContent получает данные из хтмл
-        //value получает данные из джс
+        
     }
 
     buttonEdit.addEventListener("click", openEditForm);
@@ -57,7 +56,7 @@ let formEdit = document.querySelector(".edit");
     
     closeEdit.addEventListener("click", closeEditForm);
    
-let formElement = document.querySelector(".form");
+let editFormElement = document.getElementById("editForm");
 
 
 function handleProfileFormSubmit(evt) {
@@ -73,10 +72,9 @@ function handleProfileFormSubmit(evt) {
     closeEditForm();
 
 }
+editFormElement.addEventListener('submit', handleProfileFormSubmit);
 
-//Add New Place
-
-formElement.addEventListener('submit', handleProfileFormSubmit);
+//add a new place
 
 let buttonAdd = document.querySelector(".profile__add-button");
 let closeAdd = document.querySelector(".add__close");
@@ -85,52 +83,44 @@ let formAdd = document.querySelector(".add");
 
     function openAddForm(){
         formAdd.classList.toggle("add_enabled");
-        //let fullname = document.querySelector(".profile__name"); //данные из самого сайта
-        //let job = document.querySelector(".profile__category");
-        let titleInput = document.getElementById("title"); //данные из формы
-        let linkInput = document.getElementById("link"); 
+        
+       // let titleInput = document.getElementById("title"); 
+        //let linkInput = document.getElementById("link"); 
 
-       // titleInput.value = //fullname.textContent;  //автозаполнение формы содержимым из хтмл
-        //linkInput.value = //job.textContent;
-
-        //textContent получает данные из хтмл
-        //value получает данные из джс
     }
 
     buttonAdd.addEventListener("click", openAddForm);
     
-    function closeAddForm(){
+    function closeAddFormSubmit(){
         formAdd.classList.toggle("add_enabled");
     }
     
-    closeAdd.addEventListener("click", closeAddForm);
+    closeAdd.addEventListener("click", closeAddFormSubmit);
    
+ let addFormElement = document.getElementById("addForm");
+
+function handleAddFormSubmit(evt) {
+
+  evt.preventDefault();
+  let titleInput = document.getElementById("title"); 
+  let linkInput = document.getElementById("link"); 
+
+    cardGrid.prepend(cardList({name: titleInput.value, link: linkInput.value }));
+    closeAddFormSubmit();
+
+}
 
 
-//function handleProfileFormSubmit(evt) {
+addFormElement.addEventListener('submit', handleAddFormSubmit);
 
-  //evt.preventDefault();
-    //let nameInput = document.getElementById("name");
-    //let jobInput = document.getElementById("category"); 
-    //let fullname = document.querySelector(".profile__name"); 
-    //let job = document.querySelector(".profile__category");
+//cardRender
 
-    //fullname.textContent = nameInput.value;
-    //job.textContent = jobInput.value;
-    //closeAddForm();
-
-//}
-
-
-//formElement.addEventListener('submit', handleProfileFormSubmit);
 const cardGrid = document.querySelector(".grid__cards");
 
 function cardList(card){
-  //const cardElements = initialCards.map(card => {
-  //return card.link, card.name;
-  //})
+  
    
-    const cardTemplate = document.querySelector("#card").content//.querySelector("grid__card");
+    const cardTemplate = document.querySelector("#card").content;
     
       const cardElement = cardTemplate.cloneNode(true);
       const cardImage = cardElement.querySelector(".card__image");
