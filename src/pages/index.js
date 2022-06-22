de4f72dad6d91988ec83f51902a08419e5f9b323
Daniel Evgrafov <danyaevgrafov@gmail.com>
2022-06-22 19:24:17 +0300

    add webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-//import "./index.css";
+import "./index.css";
 import { Section } from "../scripts/components/Section.js";
 import { Card } from "../scripts/components/Card.js";
 import FormValidator from "../scripts/components/FormValidator.js";
