79924d39261017908641cbfddd4b308b7b33c19f
Daniel Evgrafov <danyaevgrafov@gmail.com>
2022-06-24 18:04:59 +0300

    fix first review

--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -1,25 +1,29 @@
 export class Section {
-    constructor({ data, renderer },  containerSelector) {
-      this._items = data;
+    constructor({ item, renderer },  containerSelector) {
+      this._items = item;
       this._renderer = renderer;
       this._container = document.querySelector(containerSelector);
     }
-  
+
     addItem(data) {
-     const element = this._renderer(data);
-      this._container.prepend(element);
+      const element = this._renderer(data);
+       this._container.prepend(element);
+     }
+
+    renderItems() {
+      //this.clear();
+  
+      this._items.forEach(item => {
+      this.renderer(item);
+      });
     }
+  
+}
   
     //clear() {
      // this._container.innerHTML = " ";
     //}
   
-    renderItems() {
-      //this.clear();
-  
-      this._renderedItems.forEach(item => {
-      this._renderer(item);
-      });
-    }
-  }
+    
+ 
   
